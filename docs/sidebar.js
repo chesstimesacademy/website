1558debde293fc6988776356b2a40ee45ed69729
chesstimesacademy <support@chesstimesacademy.com>
2026-05-17 10:17:12 +0530

    Delete docs directory

--- docs/sidebar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch("sidebar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.querySelector("aside").innerHTML = data;
-    })
-    .catch(error => console.error('Error fetching sidebar:', error));
-});
